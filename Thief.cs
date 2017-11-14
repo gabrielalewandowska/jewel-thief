a89470f3ebf894a088b24d393c196ce1ceb6a01e
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-11-14 11:04:56 +0000

    Add Thief class and basic tests

---
+++ Thief.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace jewel_thief
+{
+    public class Thief
+    {
+        public int Luck { get; set; }
+        public int SocialSkills { get; set; }
+        public int Health { get; set; }
+        public int Agility { get; set; }
+        public int Strength { get; set; }
+
+        public Thief(int luck, int socialSkills, int health, int agility, int strength)
+        {
+            this.Luck = luck;
+            this.SocialSkills = socialSkills;
+            this.Health = health;
+            this.Agility = agility;
+            this.Strength = strength;
+        } 
+
+    }
+}
---
+++ ThiefTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+namespace jewel_thief
+{
+    [TestFixture()]
+    public class ThiefTest
+    {
+        private Thief thief;
+
+        [SetUp]
+        public void Init()
+        {
+            thief = new Thief(3, 0, 1, 2, 1);
+        }
+
+        [Test()]
+        public void ThiefHasLuck()
+        {
+            Assert.AreEqual(3, thief.Luck);
+        }
+
+        [Test()]
+        public void CanSetSocialSkills()
+        {
+            thief.SocialSkills = 3;
+            Assert.AreEqual(3, thief.SocialSkills);
+        }
+    }
+}
