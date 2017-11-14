b2ddbf6d89eb1f8a40b88dc68581a187930e3178
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-11-14 11:28:24 +0000

    Add ICanBeStolen interface and Jewel abstract class

---
+++ ICanBeStolen.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace jewel_thief
+{
+    public interface ICanBeStolen
+    {
+        bool Collectible { get; set; }
+    }
+}
---
+++ Jewel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace jewel_thief
+{
+    public abstract class Jewel : ICanBeStolen
+    {
+        public bool Collectible { get; set; }
+        public int Worth { get; set; }
+
+        private Jewel(int worth)
+        {
+            this.Collectible = true;
+            this.Worth = worth;
+        }
+
+    }
+}
